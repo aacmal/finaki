1448b1d415be165ecdd21f18a0a37b1953bf0359
Aca M <61030878+axcamz@users.noreply.github.com>
2022-12-16 00:07:49 +0700

    change navlink style

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -19,9 +19,9 @@ const NavLink = ({
     <Link href={href}>
       <div className={
         classNames(
-          'flex gap-4 items-center px-6 py-4 rounded-2xl transition-all duration-100',
+          'flex gap-4 items-center px-6 py-4 rounded-2xl transition-all duration-100 border border-transparent',
           {'bg-blue-500 text-white shadow-xl shadow-blue-200': active},
-          {'hover:bg-blue-400 hover:text-white text-gray-900': !active}
+          {'hover:border-blue-500 hover:text-blue-500 text-gray-900': !active}
         )
       }>
         {icon}
