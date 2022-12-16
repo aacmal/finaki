a578377251bcfe6b91609b583b162250d2c62d7d
Aca M <61030878+axcamz@users.noreply.github.com>
2022-12-16 13:58:55 +0700

    Add header and add color in bar chart

--- components/Charts/BarChart.tsx
+++ components/Charts/BarChart.tsx
@@ -8,11 +8,23 @@ type Props = {
   data: any[]
 }
 
+const COLOR = {
+  'INCOME': '#0088FE',
+  'OUTCOME': '#FF8042'
+}
+
 const BarChartHeader = () => (
   <div className='flex justify-between px-4 items-center mb-5'>
-    <h3 className='font-bold text-xl'>Pengeluaran</h3>
-    <div className='flex items-center'>
-      <div>7 Hari</div>
+    <h3 className='font-bold text-xl'>Perbandingan</h3>
+    <div className='flex items-center gap-4'>
+      <div className='flex items-center'>
+        <div style={{backgroundColor: COLOR['INCOME']}} className='h-2 w-2 rounded-full mr-2'></div>
+        <span>Pemasukan</span>
+      </div>
+      <div className='flex items-center'>
+        <div style={{backgroundColor: COLOR['OUTCOME']}} className='h-2 w-2 rounded-full mr-2'></div>
+        <span>Pengeluaran</span>
+      </div>
     </div>
   </div>
 )
@@ -23,16 +35,15 @@ const BarChart = ({ data }: Props) => {
     <ChartWrapper
       chartHeader={<BarChartHeader />}
       className='w-[30rem]'
+      height={250}
     >
       <BaChart
-        width={500}
-        height={300}
         data={data}
       >
         <CartesianGrid vertical={false} />
-        <Tooltip/>
-        <Bar isAnimationActive radius={[10, 10, 0, 0]} barSize={8} dataKey="pv" fill="#8884d8" />
-        <Bar isAnimationActive radius={[10, 10, 0, 0]} barSize={8} dataKey="uv" fill="#82ca9d" />
+        <Tooltip cursor={{fill: '#0088FE', opacity: 0.1}}/>
+        <Bar isAnimationActive radius={[10, 10, 10, 10]} barSize={8} dataKey="uv" fill="#0088FE" />
+        <Bar isAnimationActive radius={[10, 10, 10, 10]} barSize={8} dataKey="pv" fill="#FF8042" />
       </BaChart>
     </ChartWrapper>
   )
