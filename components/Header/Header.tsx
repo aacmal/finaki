69ee13142813a940b45dbdf1057783d5e70ae6c4
Aca M <61030878+axcamz@users.noreply.github.com>
2022-12-17 22:27:44 +0700

    Add margin to Navbar and Header components

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -21,7 +21,7 @@ const Header = (props: Props) => {
     }
   }
   return (
-    <header className='flex justify-between mt-3 items-center w-full px-5'>
+    <header className='flex justify-between my-4 items-center w-full px-5'>
       <h1 className='text-2xl font-bold'>{getHeaderTitle(pathname)}</h1>
       <div className='flex gap-2 items-center'>
         <div className='h-10 w-10 rounded-full bg-gray-300'></div>
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = (props: Props) => {
   
 
   return (
-    <nav className='flex justify-between lg:justify-start lg:flex-col gap-4 h-fit fixed lg:static bottom-0 p-3 lg:p-0 shadow-2xl lg:shadow-none shadow-gray-700 bg-white z-50 left-0 w-screen lg:w-fit'>
+    <nav className='flex justify-between lg:justify-start lg:flex-col gap-4 h-fit fixed lg:static bottom-0 p-3 lg:p-0 shadow-2xl lg:shadow-none shadow-gray-700 bg-white z-50 left-0 w-screen lg:w-fit lg:mt-20'>
       {/* <h1 className='mb-10 font-bold text-3xl ml-3 mt-3 hidden lg:block'>Finaki.</h1> */}
       <NavLink 
         icon={<GridIcon {...defaultIconProps}/>} 
