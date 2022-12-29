0d743314983476a1cd60112abe00d4c738867deb
Aca M <61030878+axcamz@users.noreply.github.com>
2022-12-29 23:39:21 +0700

    change redirect function from app/pages.tsx to next.config.js

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-type Props = {}
-
-const page = (props: Props) => {
-  redirect('/app/dashboard')
-}
-
-export default page
--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/app',
+        destination: '/app/dashboard',
+        permanent: true,
+      },
+    ]
+  },
   experimental: {
     appDir: true,
   },
