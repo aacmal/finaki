f8967d3716493ce3c8396dc713d2814ad6a48580
Aca M <61030878+axcamz@users.noreply.github.com>
2023-01-09 23:57:51 +0700

    added endpoints for transaction

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -32,4 +32,46 @@ async function createTransaction(req: Request, res: Response) {
   }
 }
 
-export { getAllTransactions, createTransaction };
+async function updateTransaction(req: Request, res: Response) {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({ errors: error.array() });
+  }
+
+  try {
+    const transaction = await Transaction.findById(req.query.id);
+    if (transaction) {
+      const updatedTransaction = await Transaction.updateOne({ _id: req.query.id }, { $set: req.body });
+      res.json(updatedTransaction);
+    } else {
+      res.status(404).json({ message: "Transaction not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function deleteTransaction(req: Request, res: Response) {
+  try {
+    const transaction = await Transaction.findById(req.query.id);
+    if (transaction) {
+      await transaction.remove();
+      res.json({ message: "Transaction removed" });
+    } else {
+      res.status(404).json({ message: "Transaction not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function getTransactionById(req: Request, res: Response) {
+  try {
+    const transaction = Transaction.findById(req.params.id);
+    res.json(transaction);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export { getAllTransactions, createTransaction, updateTransaction, deleteTransaction, getTransactionById };
--- src/routes/TransactionRoute.ts
+++ src/routes/TransactionRoute.ts
@@ -4,7 +4,10 @@ import { transactionValidator } from "../middlewares/validator";
 
 const route = express.Router();
 
-route.post("/add", transactionValidator, TransactionController.createTransaction);
 route.get("/", TransactionController.getAllTransactions);
+route.get("/:id", TransactionController.getTransactionById);
+route.post("/add", transactionValidator, TransactionController.createTransaction);
+route.put("/update", transactionValidator, TransactionController.updateTransaction);
+route.delete("/delete", TransactionController.deleteTransaction);
 
 export const TransactionRoute = route;
