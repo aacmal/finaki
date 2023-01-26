75090395ca4adc63bba0c0c0fe2c8ac4240132af
Aca M <61030878+axcamz@users.noreply.github.com>
2023-01-26 12:35:23 +0700

    fix api BASE_URL

--- utils/api/config.ts
+++ utils/api/config.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
-
-export const BASE_URL = "http://localhost:3001/api"; // local server
-// export const BASE_URL = "https://finaki-backend-git-test-axcamz.vercel.app/api"; // test server
-// export const BASE_URL = "https://finaki-backend.acmal.me/api"; // production server
+// const BASE_URL = "https://finaki-backend-git-test-axcamz.vercel.app/api"; // test server
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://finaki-backend.acmal.me/api"
+    : "http://localhost:3000/api";
 
 export const makeUrl = (path: string, parameters: any) => {
   if (!parameters) return path;
