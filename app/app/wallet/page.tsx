b4dadde929e88f8e54de165a43f33d10ef65df06
Aca M <61030878+axcamz@users.noreply.github.com>
2023-01-27 07:37:17 +0700

    fix text color on darkmode

--- app/app/wallet/page.tsx
+++ app/app/wallet/page.tsx
@@ -7,7 +7,7 @@ const WalletPage = (props: Props) => {
   return (
     <div className="mt-6">
       <div className="flex flex-col md:flex-row justify-between gap-3">
-        <div className="font-semibold">
+        <div className="font-semibold text-stone-700 dark:text-slate-200">
           Total saldo : <br />
           <span className="text-2xl">Rp 4.000.000</span>
         </div>
@@ -18,7 +18,7 @@ const WalletPage = (props: Props) => {
       <div className="mt-6 grid grid-cols-1  md:grid-cols-2  lg:grid-cols-3 gap-5">
         <WalletCard name="Dompet Utama" color="red" balance={1000000} />
         <WalletCard name="Tunai" color="orange" balance={1000000} />
-        <WalletCard name="BCA" color="blue" balance={1000000} />
+        <WalletCard name="BCA" color="green" balance={1000000} />
         <WalletCard name="OVO" color="purple" balance={1000000} isDefault />
       </div>
     </div>
