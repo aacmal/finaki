45d3215c709dd8be574742ca51a757db63fee300
Aca M <acamaulana123@gmail.com>
2023-07-02 18:09:36 +0700

    fix: cors config

--- dist/src/configs/cors.config.js
+++ dist/src/configs/cors.config.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     origin: [
-        "http://localhost:3000",
         "http://192.168.137.1:3000",
         "https://finaki.acmal.me",
-        "https://finaki-frontend-git-test-axcamz.vercel.app/", // test
+        "https://finaki-frontend-git-test-axcamz.vercel.app/",
+        "https://finaki.vercel.app", // vercel domain
     ],
     credentials: true,
 };
--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,9 +1,9 @@
 export const corsConfig = {
   origin: [
-    "http://localhost:3000", // local
     "http://192.168.137.1:3000", // wireless local network
     "https://finaki.acmal.me", // production
     "https://finaki-frontend-git-test-axcamz.vercel.app/", // test
+    "https://finaki.vercel.app", // vercel domain
   ],
   credentials: true,
 };
