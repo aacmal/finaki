3e529871aabfe7012c8ced504d86b67b8453dad0
Aca M <acamaulana123@gmail.com>
2023-07-08 21:17:10 +0700

    fix cors error

--- dist/src/configs/cors.config.js
+++ dist/src/configs/cors.config.js
@@ -3,10 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     origin: [
-        "http://192.168.137.1:3000",
-        "https://finaki.acmal.me",
-        "https://finaki-frontend-git-test-axcamz.vercel.app/",
-        "https://finaki.vercel.app", // vercel domain
+        "https://finaki.vercel.app",
+        "http://localhost:3000", // development
     ],
     credentials: true,
 };
--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,9 +1,7 @@
 export const corsConfig = {
   origin: [
-    "http://192.168.137.1:3000", // wireless local network
-    "https://finaki.acmal.me", // production
-    "https://finaki-frontend-git-test-axcamz.vercel.app/", // test
     "https://finaki.vercel.app", // vercel domain
+    "http://localhost:3000", // development
   ],
   credentials: true,
 };
